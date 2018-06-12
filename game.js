81c8027a311d9fe2854bb721f31966f534052346
sasrous <n.casascrous@gmail.com>
2018-06-12 20:39:36 +0200

    row moves up, fixed some issues

--- game.js
+++ game.js
@@ -1,4 +1,154 @@
 
+
+
+function Game(options){this.ctx = options.ctx;};
+var movementImput = 'l';
+var sizeIndex = 30;
+var canvas = document.getElementById('map');
+
+Game.prototype.update = function update(){
+this.ctx.clearRect(0, 0, canvas.width, canvas.height);//clean canvas
+this._drawSurface(map1);
+this._drawPJ(map1);
+}
+
+Game.prototype.start = function (){
+ this._drawSurface(map1);
+ this._drawPJ(map1);
+ this._assignEventsToKeys();
+}
+
+Game.prototype._drawSurface = function(arr){ 
+  for (var columnIndex=0; columnIndex< 16; columnIndex++){
+   for(var rowIndex=0; rowIndex<16 ;rowIndex++){
+     
+     if ((arr[rowIndex])[columnIndex] !== 0){
+      
+       this.ctx.fillStyle = this.ctx.createPattern(renders.surfaceTexture, 'repeat');
+       this.ctx.fillRect(columnIndex * sizeIndex, rowIndex  * sizeIndex, sizeIndex, sizeIndex); }
+  }}}
+
+
+Game.prototype._drawPJ = function(arr){ 
+    for (var columnIndex=0; columnIndex<16; columnIndex++){
+     for(var rowIndex=0; rowIndex<16;rowIndex++){
+       if ((arr[rowIndex])[columnIndex] === 'p'){
+         this.ctx.fillStyle = 'blue';
+         this.ctx.fillRect(columnIndex * sizeIndex, rowIndex  * sizeIndex, sizeIndex, sizeIndex); }
+  }}}
+
+
+Game.prototype._PJmove = function(arr){
+switch (movementImput){
+case 'l': 
+for (var columnIndex=0; columnIndex<16; columnIndex++){
+  for(var rowIndex=0; rowIndex<16;rowIndex++){
+    //find the PJ and check if there is space to move left
+    if ((arr[rowIndex])[columnIndex] === 'p' && ((arr[rowIndex])[columnIndex - 1] != x )){ 
+      arr[rowIndex][columnIndex]= 0 //delete the PJ from current position
+      arr[rowIndex][columnIndex - 1]='p' //move PJ to new position
+      this._checkGravity(arr); 
+      
+      this.update(); //paint current map status
+      return
+    } 
+  }} break;
+
+
+case'r': 
+for (var columnIndex=0; columnIndex<16; columnIndex++){
+  for(var rowIndex=0; rowIndex<16;rowIndex++){
+    if ((arr[rowIndex])[columnIndex] === 'p' && ((arr[rowIndex])[columnIndex + 1] != x )){
+      arr[rowIndex][columnIndex]= 0
+      arr[rowIndex][columnIndex + 1]='p'
+      this._checkGravity(arr);
+      
+      this.update(); //paint current map status
+      return
+    } 
+  }} break;
+  
+case 'up': 
+for (var columnIndex=0; columnIndex<16; columnIndex++){
+for(var rowIndex=0; rowIndex<16;rowIndex++){
+  if ((arr[rowIndex])[columnIndex] === 'p' ){ //find PJ 
+    this._moveColumnUp(columnIndex, arr); //move row up
+    arr[rowIndex-1][columnIndex]='p' // move PJ up
+    
+    this.update(); //paint current map status
+    return}
+}}  break;
+
+}}
+
+Game.prototype.goUp = function (){
+  movementImput = 'up';
+  this._PJmove(map1);
+  }
+
+Game.prototype.goLeft = function (){
+  
+    movementImput = 'l';
+    this._PJmove(map1);
+  }
+  
+Game.prototype.goRight = function (){
+
+  movementImput = 'r';
+  this._PJmove(map1);
+    }
+ 
+Game.prototype._assignEventsToKeys = function(){
+  
+  document.onkeydown = function (e){
+    
+    switch (e.keyCode){
+      case 38: //arrow up 
+      this.goUp();
+      
+      break;
+     
+      case 37: //arrow left
+      this.goLeft();
+      break;
+      case 39: //arrow right
+      this.goRight();
+      break;
+  
+    }
+  }.bind(this);
+}
+
+
+Game.prototype._checkGravity = function(arr){
+
+  for (var columnIndex=0; columnIndex<16; columnIndex++){
+     
+    for(var rowIndex=0; rowIndex<16;rowIndex++){
+      if ((arr[rowIndex])[columnIndex] === 'p' ){
+        while ((arr[rowIndex +1 ])[columnIndex] === 0){ 
+        arr[rowIndex][columnIndex]= 0
+        arr[rowIndex +1][columnIndex]='p';
+        
+        this.update();
+        
+      } 
+    }};
+    
+
+}}
+
+
+Game.prototype._moveColumnUp = function (column, map){
+ 
+  for (var rowIndex = 0; rowIndex<16 ; rowIndex++){
+     if(map[rowIndex][column] === x){
+       map[rowIndex][column]= 0
+       map[rowIndex-1][column]=x;
+       this.update();}}
+  
+};
+/*
 function Game(options){
  this.astronaut = undefined; //
  this.rows = options.rows;
@@ -12,17 +162,17 @@ Game.prototype._drawBoard = function (){
    for (var rowIndex = 0; rowIndex < this.rows; rowIndex++){
    
     this.ctx.fillStyle = this.ctx.createPattern(renders.backgroundTexture, 'repeat');
-     this.ctx.fillRect(columnIndex * 20, rowIndex  * 20, 20, 20);
+    this.ctx.fillRect(columnIndex * 20, rowIndex  * 20, 20, 20);
    }
  }
 }
 
 Game.prototype._drawSurface = function(arr){ 
-for (var columnIndex=0; columnIndex<20; columnIndex++){
+for (var columnIndex=0; columnIndex< this.columns; columnIndex++){
  
- for(var rowIndex=0; rowIndex<20;rowIndex++){
+ for(var rowIndex=0; rowIndex<arr.lenght ;rowIndex++){
    
-   if ((arr[rowIndex])[columnIndex] != 0){
+   if ((arr[rowIndex])[columnIndex] !== 0){
      
 
     
@@ -43,21 +193,43 @@ Game.prototype.start = function (){
 }
 
 
-Game.prototype._moveColumnUp = function (column){
+Game.prototype._moveColumnUp = function (column, map, ){
+ 
   
-   for (var rowIndex = this.rows; rowIndex<10 ; rowIndex--){
-     var columnIndex = this.columns;
-    if(map1[rowIndex][columnIndex] === x){
-      map1[rowIndex][columnIndex]= 0
-      map1[rowIndex][columnIndex + 1]= 0}
-      if(rowIndex>1 )  { map1[rowIndex-1][columnIndex]=x;
-        map1[rowIndex-1][columnIndex + 1]=x; }
-  this._drawBoard();
-    this._drawSurface();
-    }
-  }
+  //  for (var rowIndex = 0; rowIndex<20 ; rowIndex++){
+  //    console.log("hello2")
+     
+  //   if(map[rowIndex][column] === x){
+  //     console.log("hello")
+  //     map[rowIndex][column]= 0
+  //     map[rowIndex][column + 1]= 0
 
+  //     map[rowIndex-1][column]=x;
+  //     map[rowIndex-1][column +1]=x;
+  //     map[rowIndex-2][column]=x; 
+  //     map[rowIndex-2][column +1]=x;
+  
+  //   this._drawSurface(map1);
+  //   this._drawPJ(PJmap1);
+  
+    
+  //   }
+  // }
+  // for (var rowIndex = 0; rowIndex<20 ; rowIndex++){
+  //   console.log("hello2")
+    
+  //  if(PJmap1[rowIndex][column] === x){
+  //    console.log("hello")
+  //    PJmap1[rowIndex][column]= 0
+    
 
+  //    PJmap1[rowIndex-1][column]=x;
+     
+ 
+  //  this._drawSurface(map1);
+  //  this._drawPJ(PJmap1);}
+};
+  
   Game.prototype._drawPJ = function(arr){ 
     for (var columnIndex=0; columnIndex<10; columnIndex++){
      
@@ -110,7 +282,23 @@ for (var columnIndex=0; columnIndex<10; columnIndex++){
     } 
   }} break;
   
-case 'up': this._moveColumnUp(columnIndex);
+case 'up': 
+for (var columnIndex=0; columnIndex<10; columnIndex++){
+for(var rowIndex=0; rowIndex<10;rowIndex++){
+  if ((arr[rowIndex])[columnIndex] === 'p' && ((arr[rowIndex -1 ])[columnIndex] != x )){
+    this._moveColumnUp(columnIndex, map1);
+    PJmap1[rowIndex][columnIndex]= 0
+    PJmap1[rowIndex-1][columnIndex]='p'
+this.ctx.fillStyle = this.ctx.createPattern(renders.backgroundTexture, 'repeat');
+this.ctx.fillRect(columnIndex * 40, rowIndex  * 40, 40, 40)
+
+this._drawSurface(map1);
+this._drawPJ(PJmap1);
+  }
+}
+}
+break;
+
 this._drawSurface(map1);
 this._drawPJ(PJmap1);
 
@@ -180,3 +368,5 @@ Game.prototype._checkGravity = function(arr){
     
 
 }
+
+*/
--- main.js
+++ main.js
@@ -1,5 +1,40 @@
- 
+window.onload = function(){
+  var canvas = document.getElementById('map');
+  var ctx = canvas.getContext('2d');
+  var game = new Game({
+    ctx: ctx,
+  });
 
+  game.start();
+}
+
+
+x = undefined;
+p = undefined;
+//mapa 1 
+var map1 =
+[//2,3,4,5,6,7,8,9,1,1,2,3,4,5,6,7,8,9,2
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],//rows
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,'p',x,x],
+[0,0,0,0,x,x,x,x,x,x,x,x,x,x,x,x],
+[0,0,0,0,x,x,x,x,x,x,x,x,x,x,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,x,x],
+[x,x,x,x,0,0,0,0,0,0,0,0,0,0,x,x],
+[x,x,x,x,0,0,0,0,0,0,0,0,0,0,x,x],
+[x,x,x,x,0,0,0,0,0,0,0,0,x,x,x,x],
+[x,x,x,x,0,0,0,0,0,0,0,0,x,x,x,x],
+]
+
+ 
+ 
+ /* 
 window.onload = function(){
   console.log(document.getElementById('map'))
   var canvas = document.getElementById('map');
@@ -10,6 +45,8 @@ window.onload = function(){
     columns: canvas.height /20,
     ctx: ctx,
   });
+
+  console.log(game.rows, game.columns);
 
   game.start();
 }
@@ -59,3 +96,4 @@ var PJmap1 = [
 
 
 
+ */
